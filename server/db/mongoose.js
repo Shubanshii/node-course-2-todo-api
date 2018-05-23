b7c6145d68cf387e1f93af21e2244d9d1dbc051b
Chris Day <c_day@alumni.baylor.edu>
2018-05-23 16:54:25 -0500

    Set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = 'mongodb://Shubanshii:Baylorprser2011@ds133360.mlab.com:33360/node-todo-api';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
   useMongoClient: true
 });
 
