d6f06b42a25659146341724a371c3ac26cfd3c9e
Chris Day <c_day@alumni.baylor.edu>
2018-05-24 13:31:00 -0500

    Correct mongodb URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://Shubanshii:Baylorprser2011@ds133360.mlab.com:33360/node-todo-api';
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+mongoose.connect('mongodb://Shubanshii:Baylorprser2011@ds133360.mlab.com:33360/node-todo-api' || 'mongodb://localhost:27017/TodoApp', {
   useMongoClient: true
 });
 
