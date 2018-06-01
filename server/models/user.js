3d2720b51704a6009aed6165e6128796b799f78c
Chris Day <c_day@alumni.baylor.edu>
2018-06-01 13:18:18 -0500

    Need help debugging login route

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,27 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        console.log(res);
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   let User = this;
   let decoded;
--- server/server.js
+++ server/server.js
@@ -114,6 +114,17 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  res.send(body);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
